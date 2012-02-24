03005672210f83f02b8cb2849c3035f07cf8a379
rapid7 <rapid7@4d416f70-5f16-0410-b530-b9f4589650da>
2012-02-24 23:43:06 +0000

    Merge branch 'master' of github.com:rapid7/metasploit-framework into HEAD
    git-svn-id: https://www.metasploit.com/svn/framework3/trunk@14809 4d416f70-5f16-0410-b530-b9f4589650da

--- external/source/meterpreter/source/common/arch/posix/scheduler.c
+++ external/source/meterpreter/source/common/arch/posix/scheduler.c
@@ -239,8 +239,8 @@ scheduler_run(THREAD *thread)
 			// XXX I'd prefer to use pthread_cond_timedwait, but it's broken in bionic and just
 			// chews cpu
 
-			dprintf(" Waiting for conditional (%08x). %d vs %d",
-				&scheduler_cond, LIST_EMPTY(&WEHead), polltable == NULL);
+			//dprintf(" Waiting for conditional (%08x). %d vs %d",
+			//	&scheduler_cond, LIST_EMPTY(&WEHead), polltable == NULL);
 
 			pthread_cond_wait(&scheduler_cond, &scheduler_mutex);
 		}
